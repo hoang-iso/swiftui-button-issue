eef9b7fd336ca465e6343b0855e9f9e442b1e299
Hoang Pham <hoangpham@swiftcode.me>
2023-03-08 15:05:22 +0700

    initial commit

---
+++ ClipShapeBug/ClipShapeBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipShapeBugApp.swift
+//  ClipShapeBug
+//
+//  Created by Hoang Pham on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClipShapeBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipShapeBug/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  ClipShapeBug
+//
+//  Created by Hoang Pham on 08/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var offsetX: CGFloat = 0
+    @State private var offsetY: CGFloat = 0
+    @State private var didStep1: Bool = false
+    @State private var didStep2: Bool = false
+    
+    var body: some View {
+        renderMain()
+    }
+    
+    @ViewBuilder
+    private func renderMain() -> some View {
+        NavigationView { // Required for bug
+            VStack {
+                Button {
+                    didStep1 = true
+                } label: {
+                    Text("(1) Tap me. I'm tappable!")
+                        .padding()
+                        .background(Color.yellow)
+                }
+                if !didStep2 {
+                    Button("(2) Move to the right") {
+                        withAnimation {
+                            offsetX = 100
+                        }
+                        didStep2 = true
+                    }
+                    .foregroundColor(.red)
+                    .padding()
+                } else {
+                    Text("(3) Hit area messed up!\nNow you can tap on the left\nof the button above!")
+                }
+            }
+            .navigationViewStyle(.stack)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .cornerRadius(20)  // Required for bug
+        .background(.blue)
+        .offset(x: offsetX, y: offsetY)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
